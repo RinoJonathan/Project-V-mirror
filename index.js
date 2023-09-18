ee3dc3c08022290f89bddb3d4354a2ff7f446ab0
JustinFrost47 <rinojonathanarul@gmail.com>
2023-09-18 19:57:22 +0530

    added codes for error handling

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
+const appError = require("./utilities/appError")
+const asyncWrapper = require("./utilities/asyncWrapper")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -17,20 +19,36 @@ app.set("views", path.join(__dirname, "views"));
 
 
 
+
 const port =  3000;
 
 app.get("/", (req, res) => {
-
+    
     res.render("pages/homepage");
 })
 
 
 
 
-app.get("*", (req, res) => {
 
-    res.send("Route not found");
+
+//404 route
+
+app.all("*", (req, res) => {
+    throw new appError("page not found", 404)
 })
+
+//error handling route
+
+app.use((err, req, res, next) => {
+    
+    const { message= "something went wrong", status= 500, stack } = err
+
+    console.log(`${status}  ${message}`)
+    res.status(status).render("error", { status, message, stack})
+})
+
+
 
 app.listen(port, () => {
     console.log(`listening from port: ${port} `)
