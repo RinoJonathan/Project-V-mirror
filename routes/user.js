90a16d27de9068801890039033a288da2c488bce
JustinFrost47 <rinojonathanarul@gmail.com>
2023-10-03 00:43:44 +0530

    reset user.js

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,1 @@
-const mongoose = require('mongoose')
-const passportMongo = require('passport-local-mongoose')
-
-const userSchema = mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    }
-})
-
-userSchema.plugin(passportMongo)
-
-module.exports = mongoose.models('user', userSchema)
+ 
