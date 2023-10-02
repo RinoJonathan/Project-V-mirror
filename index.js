15bcd3680be3d9cf314a31f84a06ce455ee5306f
JustinFrost47 <rinojonathanarul@gmail.com>
2023-10-02 22:37:46 +0530

    added public  directory for static files

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ app.engine('ejs', ejsMate);
 app.set("view engine", 'ejs');
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, "public")))
+
 
 //routes
 const testRoutes = require('./routes/test')
