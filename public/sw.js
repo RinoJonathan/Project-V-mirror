6a23d56cba25745705cd8189100abca76193617c
JustinFrost47 <rinojonathanarul@gmail.com>
2023-10-08 16:54:48 +0530

    caching strategy tested - working alright

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Define a version for your cache (update this when you make changes).
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'vt0.009';
 
 // Define a cache name based on the version.
 const CACHE_NAME = `Project-V-cache-${CACHE_VERSION}`;
@@ -8,29 +8,33 @@ const CACHE_NAME = `Project-V-cache-${CACHE_VERSION}`;
 // Define an array of static resources to cache.
 const APP_STATIC_RESOURCES = [
   '/',
-  '/index.html',
-  '/script.js',
-  '/style.css',
-  '/icons/wheel.svg',
+  '/css/style.css',
+  '/javascript/script.js',
+  '/javascript/feature_script.js',
+  '/offline.html',
+  'offlineLogin.html',
   // Add more static resources here as needed.
 ];
 
 const FEATURE_RESOURCES = [
-  '/javascript/ffmpeg/'
+  '/temp.js',
 ]
 
 //function to check if user had logged in online
 const checkAuth = () => {
     
 
-  const jwtToken = document.cookie.split('; ').find((cookie) => cookie.startsWith('jwt='));
+  // const jwtToken = document.cookie.split('; ').find((cookie) => cookie.startsWith('jwt='));
+  const jwtToken = true
   
   if (jwtToken) {
     
+    console.log("jwt available")
     return true;
 
   } else {
     
+    console.log("no jwt available")
     return false;
 
   }
@@ -39,9 +43,14 @@ const checkAuth = () => {
 // Function to clean up old caches when activating a new service worker.
 const cleanUpCaches= () => {
   return caches.keys().then((cacheNames) => {
+    console.log(cacheNames)
     return Promise.all(
+      
       cacheNames.map((cache) => {
-        if (cache !== CACHE_NAME) {
+        console.log("iteration1")
+        if (cache !== CACHE_NAME)
+         {
+          console.log(`${cache} deleted`)
           return caches.delete(cache);
         }
       })
@@ -67,12 +76,24 @@ self.addEventListener("install", (event) => {
 
 
   
-self.addEventListener('activate', (event) => {
-  // Clean up old caches when activating a new service worker.
-  event.waitUntil(cleanUpCaches());
-
-  // Ensure the service worker takes control of the pages immediately.
-  self.clients.claim();
+// delete old caches on activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      console.log(names)
+      await Promise.all(
+        names.map((name) => {
+          console.log("iteration1")
+          if (name !== CACHE_NAME) {
+            console.log(`${name} deleted`)
+            return caches.delete(name);
+          }
+        })
+      );
+      await clients.claim();
+    })()
+  );
 });
 
 self.addEventListener('fetch', (event) => {
@@ -80,15 +101,18 @@ self.addEventListener('fetch', (event) => {
   const urlObj = new URL(request.url);
   const url = urlObj.pathname.toLowerCase()
 
+  // console.log(url)
   // Handle homepage caching 
   if (url === '/') {
     // Check if the user is online.
     if (navigator.onLine) {
       // If online, fetch the resource from the network (do not cache).
+      console.log("online home")
       event.respondWith(fetch(request));
       return;
     } else {
       // If offline, respond with the cached page.
+      console.log("offline home")
       event.respondWith(caches.match(request));
       return;
     }
@@ -96,6 +120,7 @@ self.addEventListener('fetch', (event) => {
 
   // Handle static resource caching (always cache).
   if (APP_STATIC_RESOURCES.includes(url)) {
+    console.log("offline static caches")
     event.respondWith(caches.match(request));
     return;
   }
@@ -104,32 +129,67 @@ self.addEventListener('fetch', (event) => {
   // For example, you can check if the user is authenticated by verifying
   // the presence and validity of a JWT token stored on the client.
 
-  if (checkAuth()) {
-    // Handle authenticated feature pages.
-    if (url.startsWith('/features/')) {
+   
+  // Handle authenticated feature pages.
+
+
+  if (url.startsWith('/feature')) {
+
+    console.log("inside /fetch")
+    if (checkAuth()) {
+      // Handle authenticated feature pages.
+      
       event.respondWith(
         caches.match(request).then((cachedResponse) => {
-          return cachedResponse || fetch(request).then((fetchResponse) => {
-            // Cache the fetched response for future use.
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(request, fetchResponse.clone());
+          if (cachedResponse) {
+            // If cached resource is present, use it.
+            console.log("Cached resource present.");
+            return cachedResponse;
+          } 
+          else {
+            if(navigator.onLine){
+              
+              // If cached resource is not present, fetch it from the network.
+            console.log("Cached resource not present. Fetching from network...");
+            return fetch(request).then((fetchResponse) => {
+              if (fetchResponse.status === 200) {
+                // Clone the response before caching it.
+                const responseToCache = fetchResponse.clone();
+
+                // Cache the fetched response for future use.
+                caches.open(CACHE_NAME).then((cache) => {
+                  cache.put(request, responseToCache);
+                });
+              }
+
+              console.log("Fetched feature page from the network.");
+              return fetchResponse;
+            }).catch((e) => {
+              console.log("Error while fetching feature page:", e);
             });
-            return fetchResponse;
-          }).catch((e) => {
-            console.log("error while fetching feature page")
-          });
+            }
+            else{
+              console.log("turn on the network to see the feature")
+              return caches.match('/offline.html');
+            }
+          }
         })
       );
       return;
+      
     }
-  } else {
-    // Display a custom message or prompt the user to log in when offline.
-    // You can show a modal, a custom HTML element, or navigate to a login page.
-    console.log("Please log in using the internet.");
-    // If offline, respond with the cached offline.html page.
-    window.location.href = '/user/login';
-    return;
+    else {
+      // Display a custom message or prompt the user to log in when offline.
+      // You can show a modal, a custom HTML element, or navigate to a login page.
+      console.log("Please log in using the internet.");
+      // If offline, respond with the cached offline.html page.
+      window.location.href = '/user/login';
+      return;
+    }
+
+    
   }
+  
 
   if (url.startsWith('/user')) {
     // Check if the user is online.
