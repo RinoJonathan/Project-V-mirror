2732fd87f24f8c248489a30e54067b6ad04cc031
JustinFrost47 <rinojonathanarul@gmail.com>
2023-10-09 09:20:32 +0530

    migrated 7 features added routes and test link

--- routes/features.js
+++ routes/features.js
@@ -12,4 +12,34 @@ router.get('/trim', isLoggedIn, (req, res) => {
     res.render("features/trim")
 })
 
+router.get('/convert', isLoggedIn, (req, res) => {
+
+    res.render("features/convert")
+})
+
+router.get('/merge', isLoggedIn, (req, res) => {
+
+    res.render("features/merge")
+})
+
+router.get('/split', isLoggedIn, (req, res) => {
+
+    res.render("features/split")
+})
+
+router.get('/resize', isLoggedIn, (req, res) => {
+
+    res.render("features/resize")
+})
+
+router.get('/getaudio', isLoggedIn, (req, res) => {
+
+    res.render("features/getaudio")
+})
+
+router.get('/muteaudio', isLoggedIn, (req, res) => {
+
+    res.render("features/muteaudio")
+})
+
 module.exports = router
