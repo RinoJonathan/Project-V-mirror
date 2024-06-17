ecc3c32c0fb54b94a905e0f087a5e7b5df24886f
JustinFrost47 <rinojonathanarul@gmail.com>
2024-06-17 14:02:27 +0530

    created  env config

---
+++ utilities/envConfig.js
@@ -0,0 +1,8 @@
+import * as dotenv from "dotenv";
+
+// Load environment variables based on NODE_ENV
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: ".env.production" });
+} else if (process.env.NODE_ENV === "development") {
+  dotenv.config({ path: ".env.development" });
+}
